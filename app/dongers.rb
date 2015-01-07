2b1722713ac038564bc2f3017fca237fe2f21335
Assaf Gelber <assaf.gelber@gmail.com>
2015-01-07 18:26:27 +0200

    Remove 'All' and don't append trailing slash becuase dongerlist can't handle that

--- app/dongers.rb
+++ app/dongers.rb
@@ -8,18 +8,16 @@ module Dongers
   class NoDongersFound < StandardError; end
 
   HOST = 'http://dongerlist.com'
-  CATEGORY_PATH = '/category/%s'
+  CATEGORY_PATH = '/category/'
   DONGER_HTML_CLASS = '.copy-donger'
   DONGER_TEXT_ATTRIBUTE = 'data-clipboard-text'
 
-  CATEGORIES = ['All', 'Ameno', 'Angry', 'Animal', 'Brick', 'Cool', 'Cracker', 'Crazy', 'Cry', 'Cute', 'Dance', 'Dead', 'Donger', 'Dunno', 'Excited', 'Fight', 'Finger', 'Flex', 'Flip', 'Flower', 'Glasses', 'Gun', 'Happy', 'Lenny', 'Lower', 'Mad', 'Magic', 'Man', 'Meh', 'Mob', 'Monocle', 'Point', 'Pyramid', 'Raise', 'Run', 'Sad', 'Scared', 'Scary', 'Shocked', 'Spider', 'Surprised', 'Sword', 'Table', 'Throw', 'Tree', 'Ugly', 'Upset', 'Walk', 'Wall', 'Why']
+  CATEGORIES = ['Ameno', 'Angry', 'Animal', 'Brick', 'Cool', 'Cracker', 'Crazy', 'Cry', 'Cute', 'Dance', 'Dead', 'Donger', 'Dunno', 'Excited', 'Fight', 'Finger', 'Flex', 'Flip', 'Flower', 'Glasses', 'Gun', 'Happy', 'Lenny', 'Lower', 'Mad', 'Magic', 'Man', 'Meh', 'Mob', 'Monocle', 'Point', 'Pyramid', 'Raise', 'Run', 'Sad', 'Scared', 'Scary', 'Shocked', 'Spider', 'Surprised', 'Sword', 'Table', 'Throw', 'Tree', 'Ugly', 'Upset', 'Walk', 'Wall', 'Why']
 
   def self.get(category = '')
     raise NonexistentCategory.new if !category.empty? && !CATEGORIES.include?(category)
 
-    path = category ? (CATEGORY_PATH % category) : '/'
-    url = URI.join(HOST, path)
-
+    url = category.empty? ? HOST : URI.join(HOST, CATEGORY_PATH, category)
     raw_html = HTTParty.get(url, headers: {'User-Agent' => ''}).body
     dongers = Nokogiri::HTML(raw_html).css(DONGER_HTML_CLASS)
     raise NoDongersFound.new if dongers.empty?
