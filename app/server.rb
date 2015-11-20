d22d246ca9311d68231dead7e745a341049636b4
Assaf Gelber <assaf.gelber@gmail.com>
2015-11-20 23:48:07 +0200

    Update according to new slack responses

--- app/server.rb
+++ app/server.rb
@@ -1,15 +1,12 @@
 require 'sinatra'
 require_relative './dongers'
-require_relative './slackhook'
-
-SLACK_TOKEN = ENV['SLACK_TOKEN']
 
 CATEGORIES_COMMAND = 'categories'
 NONEXISTENT_CATEGORY = "That category doesn't exist ᕕ(˵•̀෴•́˵)ᕗ\nTry '/donger categories' for a list of all categories"
 NO_DONGERS_FOUND = "There aren't any dongers in that category ┌╏✖_✖╏┘"
 
 post '/donger' do
-  halt 401 unless request['token'] == SLACK_TOKEN
+  content_type :json
 
   text = request['text'].strip
   if text == CATEGORIES_COMMAND
@@ -17,12 +14,19 @@ post '/donger' do
   else
     begin
       donger = Dongers.get(text.capitalize)
-      Slackhook.post(donger, request['channel_id'])
-      nil
+      respond_in_channel donger
     rescue Dongers::NonexistentCategory
-      NONEXISTENT_CATEGORY
+      respond NONEXISTENT_CATEGORY
     rescue Dongers::NoDongersFound
-      NO_DONGERS_FOUND
+      respond NO_DONGERS_FOUND
     end
   end
+end
+
+def respond(text)
+  {text: text}
+end
+
+def respond_in_channel(text)
+  {response_type: 'in_channel', text: text}
 end
